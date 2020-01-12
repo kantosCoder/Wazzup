96274236cf5ba60e6d51d405c655db6e5d0ecf79
kantosCoder <56650488+kantosCoder@users.noreply.github.com>
2020-01-12 01:35:50 +0100

    First push
    Ajajaja c creo hel proyecto

---
+++ app/src/main/java/com/example/whazzup/Configuration.java
@@ -0,0 +1,15 @@
+package com.example.whazzup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Configuration extends AppCompatActivity {
+    //punto de entrada principal// pantalla configuracion // sergio
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.configuration_activity);
+    }
+}
---
+++ app/src/main/java/com/example/whazzup/NetCode.java
@@ -0,0 +1,15 @@
+package com.example.whazzup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NetCode extends AppCompatActivity {
+
+    //pantalla de chat// clases de conexion //hector
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_net_code);
+    }
+}
